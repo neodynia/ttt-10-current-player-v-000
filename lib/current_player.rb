361162e9f80ac02b648b2cf7d0df60d38cb0fea5
Ryan  Boris <ryan.boris@gmail.com>
2017-11-21 20:44:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,6 @@
 board = ["X", " ", "X", "0", " ", " ", " ", " ", " "]
-def turn_count(board)
+turn_count.each do |board|
   counter = 0
-  turn_count.each do |board|
   puts board.count
   counter+=1
-  end
 end
