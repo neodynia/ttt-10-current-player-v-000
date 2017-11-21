bd5c561484592293286451d9509cd0497f36cce7
Ryan  Boris <ryan.boris@gmail.com>
2017-11-21 23:50:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
-turn_count.each do |board|
+  board.each do |board| 
   counter = 0
-  array.count("X") && array.count("O")
+  array.count("X") && array.count("O") = turns
   counter+=1
   end
+  puts turns 
 end
